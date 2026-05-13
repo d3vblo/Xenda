d48e7282b32bc49f0c30b572edccc29637c56e06
Yoltic9601 <diazedgar1701@gmail.com>
2026-05-13 14:47:48 -0600

    alta PAOLA

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ def logout():
 def crear_usuario():
 
     existente = Usuario.query.filter_by(
-        correo='diazedgar1701@gmail.com'
+        correo='paolamateoponce@gmail.com'
     ).first()
 
     if existente:
