ca8bd826cb7042f9942e80c14396748e0f5eab19
Yoltic9601 <diazedgar1701@gmail.com>
2026-05-13 12:35:23 -0600

    Fix db initialization

--- app.py
+++ app.py
@@ -204,9 +204,10 @@ def registros():
 # INICIO
 # =========================================
 
-if __name__ == '__main__':
+with app.app_context():
+    db.create_all()
 
-    with app.app_context():
-        db.create_all()
+
+if __name__ == '__main__':
 
     app.run(host='0.0.0.0', port=5000, debug=True)
